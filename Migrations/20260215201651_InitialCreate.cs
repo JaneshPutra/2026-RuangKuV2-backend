1c92b0226ea60abd3beea18d1d19f307b03e3d1d
Janesh Putra <janeshputra12@gmail.com>
2026-02-16 03:18:57 +0700

    fitur : setup sqlite database (closes #1)

---
+++ Migrations/20260215201651_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RuangKuApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Peminjamans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NamaPeminjam = table.Column<string>(type: "TEXT", nullable: false),
+                    Ruangan = table.Column<string>(type: "TEXT", nullable: false),
+                    TanggalPinjam = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TanggalKembali = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Peminjamans", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Peminjamans");
+        }
+    }
+}
